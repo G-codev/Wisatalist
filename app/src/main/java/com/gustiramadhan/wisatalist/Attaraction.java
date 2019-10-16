396044f6969c2a766a7996f2741fbb92ecb91650
List view <gustiramadhan830@gmail.com>
2019-10-16 14:32:05 +0700

    IwisataList

---
+++ app/src/main/java/com/gustiramadhan/wisatalist/Attaraction.java
@@ -0,0 +1,37 @@
+package com.gustiramadhan.wisatalist;
+
+public class Attaraction {
+    int image;
+    String placeName;
+    String description;
+
+    public Attaraction(int image, String placeName, String description) {
+        this.image = image;
+        this.placeName = placeName;
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
