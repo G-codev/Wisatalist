28eb320ccf2f1337b808e1a82edd8c4a45ea7837
Wisata <gustiramadhan830@gmail.com>
2019-10-17 09:55:40 +0700

    Wisata Indonesia

---
+++ app/src/main/java/com/gustiramadhan/wisatalist/Adapter.java
@@ -0,0 +1,69 @@
+package com.gustiramadhan.wisatalist;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Adapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Attaraction> places;
+    public void setAttaraction(ArrayList<Attaraction> places){
+        this.places = places;
+    }
+    public Adapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return places.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return places.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null){
+            view = LayoutInflater.from(context)
+                    .inflate(R.layout.itemlist,viewGroup,false);
+        }
+        ViewHolder viewHolder = new ViewHolder(view);
+        Attaraction place = (Attaraction) getItem(i);
+        viewHolder.bind(place);
+
+        return view;
+    }
+
+    private class ViewHolder{
+        private TextView txtName;
+        private TextView txtDescription;
+        private ImageView imgphoto;
+
+        ViewHolder (View view){
+            txtName = view.findViewById(R.id.txtname);
+            txtDescription = view.findViewById(R.id.txt_descripsi);
+            imgphoto = view.findViewById(R.id.img_photo);
+        }
+        void  bind (Attaraction places){
+            txtName.setText(places.getPlaceName());
+            txtDescription.setText(places.getDescription());
+            imgphoto.setImageResource(places.getImage());
+
+        }
+    }
+}
--- app/src/main/java/com/gustiramadhan/wisatalist/MainActivity.java
+++ app/src/main/java/com/gustiramadhan/wisatalist/MainActivity.java
@@ -2,13 +2,30 @@ package com.gustiramadhan.wisatalist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private  String[] dataName;
+    private  String[] dataDescription;
+    private TypedArray dataPhoto;
+    private Adapter adapter;
+    private ArrayList<Attaraction> places;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new Adapter(this);
+        ListView listView = findViewById(R.id.lv_list);
+        listView.setAdapter(adapter);
+
+        prepare();
+        additem();
     }
 }
